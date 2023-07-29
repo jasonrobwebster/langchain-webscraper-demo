681fd56a039a1683ea3ce8bf0471eed5592b490b
jasonwebster <jasonrobwebster@gmail.com>
2023-07-29 23:39:24 +0200

    use original sites as sources

--- embed.py
+++ embed.py
@@ -1,22 +1,20 @@
 import os
-import argparse
+import json
+
 from langchain.document_loaders import (
     BSHTMLLoader,
     DirectoryLoader,
-    UnstructuredHTMLLoader,
 )
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import (
-    CharacterTextSplitter,
     RecursiveCharacterTextSplitter,
 )
 from langchain.vectorstores import Chroma
 
 from dotenv import load_dotenv
 
-load_dotenv()
-
 if __name__ == "__main__":
+    load_dotenv()
     if os.path.exists("./chroma"):
         print("already embedded")
         exit(0)
@@ -34,7 +32,16 @@ if __name__ == "__main__":
     )
     data = loader.load()
     documents = text_splitter.split_documents(data)
-    print(len(documents))
+
+    # map sources from file directory to web source
+    with open("./scrape/sitemap.json", "r") as f:
+        sitemap = json.loads(f.read())
+
+    for document in documents:
+        document.metadata["source"] = sitemap[
+            document.metadata["source"].replace(".html", "").replace("scrape/", "")
+        ]
+
     embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002")
     db = Chroma.from_documents(documents, embedding_model, persist_directory="./chroma")
     db.persist()
